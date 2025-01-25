391e9f63f5217eec12523b7258c3cf75bc9f9ebb
GabrielCaja <gabrielcajaegido@gmail.com>
2025-01-25 11:44:41 +0100

    Eliminar migraciones antiguas de productos y productos_categorías, y agregar nueva migración con relación a categorías.

---
+++ app/Models/Productos.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Productos extends Model
+{
+    protected $table = 'productos';
+    protected $primaryKey = 'id';
+    protected $fillable = ['nombre',"descripcion",'precio', 'stock', 'categoria_id'];
+
+    public function categoria()
+    {
+        return $this->hasMany(Categorias::class, 'categoria_id', 'id');
+    }
+}
